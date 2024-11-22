24b2ee1f7955ebedb48f3514ddf764bc7d11ee9d
euphy <738132704@qq.com>
2024-11-22 10:56:47 +0800

    feat：添加oauth2 自定义handler 事件驱动句柄

---
+++ src/main/java/org/example/handle/MyAccessDeniedHandler.java
@@ -0,0 +1,23 @@
+package org.example.handle;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+/**
+ * 自定义权限不足处理
+ */
+@Component
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().write("权限不足,请联系管理员.");
+    }
+}
